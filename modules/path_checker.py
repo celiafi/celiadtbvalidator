ab0fe7bb21f7fbeebcadc233a9e05b4a25b5c557
CeliaAdmin <admin@celia.fi>
2021-08-23 12:05:04 +0300

    Improved path checking

--- modules/path_checker.py
+++ modules/path_checker.py
@@ -8,8 +8,10 @@ class PathChecker:
     @staticmethod
     def check_audio_ext_paths():
         ffmpeg_path = ConfigGetter.get_configs("ffmpeg_path")
+        if ffmpeg_path != "ffmpeg":
+            ffmpeg_path = str(pathlib.Path(ffmpeg_path).absolute())
         if which(ffmpeg_path) is None:
-            print("FFmpeg not found at " + str(ffmpeg_path))
+            #print("FFmpeg not found at " + str(ffmpeg_path))
             return False
         else:
             return True
@@ -18,14 +20,9 @@ class PathChecker:
     def check_daisy_ext_paths():
         programs_found = True
         
-        pipeline_path = pathlib.Path(ConfigGetter.get_configs("pipeline_path"))
+        pipeline_path = pathlib.Path(ConfigGetter.get_configs("pipeline_path"), "pipeline-cli.bat").absolute()
         if not pipeline_path.exists():
             programs_found = False
-            print("Pipeline not found at " + str(pipeline_path))
-
-        java_path = ConfigGetter.get_configs("java_path")
-        if which(java_path) is None:
-            programs_found = False
-            print("Java not found at " + str(java_path))
+            #print("Pipeline not found at " + str(pipeline_path))
         
         return programs_found
