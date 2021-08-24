68791e15e38885d780cfaf7517fa7e1f7c615d22
CeliaAdmin <admin@celia.fi>
2021-08-24 12:59:59 +0300

    Remove debug printing

--- modules/audio_scrutinizer.py
+++ modules/audio_scrutinizer.py
@@ -33,7 +33,7 @@ class AudioScrutinizer:
         ffmpeg_path = ConfigGetter.get_configs("ffmpeg_path")
         if ffmpeg_path != "ffmpeg":
             ffmpeg_path = str(pathlib.Path(ffmpeg_path).absolute())
-        print("FFmpeg at " + ffmpeg_path)
+       # print("FFmpeg at " + ffmpeg_path)
         ffmpeg_cmd_lufs = 'cmd /c ""' + ffmpeg_path + '" -nostats -i "' + f_path + '" -filter_complex ebur128=peak=true -f null - 2>&1"'
         ffmpeg_cmd_stats = 'cmd /c ""' + ffmpeg_path + '" -i "' + f_path + '" -af astats -f null - 2>&1"'
         #input(ffmpeg_cmd_stats)
@@ -113,7 +113,7 @@ class AudioScrutinizer:
         ffmpeg_path = ConfigGetter.get_configs("ffmpeg_path")
         if ffmpeg_path != "ffmpeg":
             ffmpeg_path = str(pathlib.Path(ffmpeg_path).absolute())
-        print("FFmpeg at " + ffmpeg_path)
+        # print("FFmpeg at " + ffmpeg_path)
         start_silence_max_ffmpeg_cmd = 'cmd /c ""' + ffmpeg_path + '" -ss 00:00:00 -nostats -i "' + f_path + '" -to 00:0' + start_silence_max + ' -filter_complex ebur128=peak=true -f null - 2>&1"'
         
         raw_output = ExternalProgramCaller.run_external_command(start_silence_max_ffmpeg_cmd).splitlines()
